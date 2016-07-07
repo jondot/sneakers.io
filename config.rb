77b0d1371030fd068ad7ed4a24cf1404097870c6
Dotan Nahum <jondotan@gmail.com>
2016-07-07 12:14:02 +0300

    change the deploy target

--- config.rb
+++ config.rb
@@ -122,7 +122,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
   # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote   = 'git@github.com:jondot/sneakers.git'
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
